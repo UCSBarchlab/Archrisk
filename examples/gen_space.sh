5b797ae8283c5fb7afc44ee4f14d24ae0df60027
weil0ng <cuiweilong0205@gmail.com>
2017-08-24 14:19:10 -0700

    clean up to MRP

--- examples/gen_space.sh
+++
@@ -1,7 +0,0 @@
-mkdir $1
-cd $1
-
-time python ../decision_impact.py --log debug --math_model hete --risk_func quad --f 0.999 --c 0.001 $2 &> HPLC.log &
-time python ../decision_impact.py --log debug --math_model hete --risk_func quad --f 0.999 --c 0.01 $2 &> HPHC.log &
-time python ../decision_impact.py --log debug --math_model hete --risk_func quad --f 0.9 --c 0.001 $2 &> LPLC.log &
-time python ../decision_impact.py --log debug --math_model hete --risk_func quad --f 0.9 --c 0.01 $2 &> LPHC.log &
