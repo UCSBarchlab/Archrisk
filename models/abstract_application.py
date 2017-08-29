c9a240137c5ea92b139cfee58704403b6774d835
weil0ng <cuiweilong0205@gmail.com>
2017-08-29 11:44:26 -0700

    fix app

--- models/abstract_application.py
+++ models/abstract_application.py
@@ -1,5 +1,3 @@
-from utils.preprocessing import FileHelper
-
 from collections import defaultdict
 import itertools
 import logging
@@ -24,27 +22,3 @@ class App(object):
         feed_f = ('f', self.f)
         feed_c = ('c', self.c)
         return [feed_f, feed_c]
-
-class AppHelper(object):
-    @staticmethod
-    def gen_app(path=None, regress_model=None):
-        fs = [.5, .9, .99, .999]
-        cs = [.0, .001, .01, .1]
-        tag = 'Pathological'
-        if path is not None:
-            assert(regress_model)
-            data, wklds = FileHelper.read_data(path)
-        else:
-            return [App(tag, f, c) for f, c in itertools.product(fs, cs)]
-
-        if regress_model:
-            apps = []
-            for k, df in data.groupby('workload'):
-                cores = df['thread_count']
-                speedup = df['speedup']
-                result = regress_model.fit(cores, speedup)
-                if 'c' in result: 
-                    apps.append(App(k, result['f'], result['c']))
-                else:
-                    apps.append(App(k, result['f'], 0))
-            return apps
